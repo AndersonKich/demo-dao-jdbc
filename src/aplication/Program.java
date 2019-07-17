e4338bb0d6266bc2becf36b303eb02452fd6887d
Anderson Kich <kich24@hotmail.com>
2019-07-17 18:18:41 -0300

    Implementacao da Classe findById

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,20 +1,18 @@
 package aplication;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.dao.impl.SellerDaoJDBC;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
+		SellerDao sellerdao = DaoFactory.creatSellerDao();
 		
+		Seller seller = sellerdao.findById(3);
 		
-		
+		System.out.println(seller);
 		
 	}
 
